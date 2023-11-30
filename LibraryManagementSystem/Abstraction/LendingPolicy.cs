cdb753c78a1d2f9c7526b0ef9f7537b392b4166a
Kubilay Cem KARAHAN <kubilaycem@yandex.com>
2023-11-30 21:12:06 +0300

    FirstCommit

---
+++ LibraryManagementSystem/Abstraction/LendingPolicy.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibraryManagementSystem.Models;
+
+namespace LibraryManagementSystem.Abstraction
+{
+    public abstract class LendingPolicy
+    {
+        public abstract string PolicyName { get; }
+
+        public abstract void ApplyPolicy(Member member, Book book);
+    }
+
+   
+
+   
+}
---
+++ LibraryManagementSystem/Models/ShortTermLendingPolicy.cs
@@ -0,0 +1,20 @@
+﻿using LibraryManagementSystem.Abstraction;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem.Models
+{
+    public class ShortTermLendingPolicy : LendingPolicy
+    {
+        public override string PolicyName => "Short Term Lending Policy";
+
+        public override void ApplyPolicy(Member member, Book book)
+        {
+            // Kısa süreli ödünç verme politikası uygulama
+            Console.WriteLine($"'{book.Title}' kitabı, {member.FirstName} {member.LastName}'e kısa süreli olarak ödünç verildi.");
+        }
+    }
+}
